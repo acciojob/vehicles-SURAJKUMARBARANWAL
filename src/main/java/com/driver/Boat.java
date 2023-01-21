573170bf3436785804ed0e73d04d4fb521b4b448
Suraj Kumar <surajbaranwal56@gmail.com>
2023-01-21 16:16:05 +0530

    Add boad contructor

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -3,6 +3,11 @@ package com.driver;
 public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
+    public Boat(String name ,int capacity){
+        this.name=name;
+        this.capacity=capacity;
+
+    }
     public Boat(){
 
     }
